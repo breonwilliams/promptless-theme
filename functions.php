169a7950d3d18a8abdea243caa0b0d872a7e5df7
Breon Williams <contactbreonwilliams@gmail.com>
2026-07-16 08:19:35 -0400

    Release 1.2.8

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Theme version constant
  */
-define( 'PROMPTLESS_THEME_VERSION', '1.2.7' );
+define( 'PROMPTLESS_THEME_VERSION', '1.2.8' );
 
 /**
  * Theme directory path
